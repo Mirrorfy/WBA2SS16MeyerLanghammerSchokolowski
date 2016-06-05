f6bd0441c0fca883ab5f862509184e71260c4a69
Mirrorfy <daniela.meyer@smail.fh-koeln.de>
2016-06-05 11:24:07 +0200

    vollständige implementierung

--- Express.js
+++ Express.js
@@ -27,37 +27,89 @@ app.use(function (req, res, next) { // Log mit Pfad und Zeitangabe
 });
 
 // Routing
-app.post('/anhang/:ID', jsonParser, function (req, res) { // Erstellt einen neuen Anhang in der Liste; Parser wandeln den JSON String in eine entsprechende Variable
-// siehe hochladen eines anhanges
-});
 
-app.get('/liste', function (req, res) { //Abruf der Anhangsliste
-  res.send(liste.all());
-});
-
-app.post('/notiz/:ID', function (req, res) { //Anlegen einer neuen Notiz
- var notiz = notiz.create(req.body.data);  //req.body.data????
-  res.send(201, notiz);
-});
-
-app.get('/anhang/:ID', function (req, res) { //Runterladen eines Anhanges
-  res.send(anhang.withId(req.params.id));
+app.get('/anhang', function (req, res) { //Abruf der Anhangsliste
+   db.lrange('anhang:*', function(err, reply) {
+                var anhang = [];
+                reply.forEach(function(element){
+                    anhang.push(JSON.parse(element));
+                });
+                res.status(200).json(anhang);
+    });
 });
 
 
-app.post('/anhang/:ID', function (req, res) { //hochladen eines Anhanges
- var anhang = anhang.create(req.body.data);  //req.body.data????
-  res.send(201, anhang);
+app.post('/notiz/:ID',jsonParser, function (req, res) { //Notiz erstellen
+  var newNotiz = req.body;
+    
+    db.incr('id:notiz', function(err, rep){
+        newNotiz.id = rep;
+        db.set('Noitz:'+newNotiz.id, JSON.stringify(newNotiz), function(err, rep){
+            res.status(200).json(newNotiz);
+        });
+    });
 });
 
 
-app.get('/liste/:ID', function (req, res) { //Suchfunktion in der Anhangsliste
- res.send(anhang.withId(req.params.id));
+app.get('/Notiz/:ID', function (req, res) { //Notiz anzeigen
+    db.get('notiz:'+req.params.id, function(err, rep){
+        if(rep){
+            res.type('json').send(rep);
+        }
+        else{
+            res.status(404).type('text').send(Diese Notiz wurde nicht gefunden.);
+        }
+    });
 });
+
+
+app.get('/anhang/:ID', function (req, res) { //Anhang runterladen
+    db.get('anhang:'+req.params.id, function(err, rep){
+        if(rep){
+            res.type('json').send(rep);
+        }
+        else{
+            res.status(404).type('text').send(Dieser Anhang wurde nicht gefunden.);
+        }
+    });
+});
+
+
+app.post('/anhang/:ID',jsonParser, function (req, res) { //Anhang zufügen
+  var newAnhang = req.body;
+    
+    db.incr('id:anhang', function(err, rep){
+        newAnhang.id = rep;
+        db.set('Anhang:'+newAnhang.id, JSON.stringify(newAnhang), function(err, rep){
+            res.status(200).json(newAnhang);
+        });
+    });1
+});
+
+//ist dies noch nötig, da es sich ja um das gleiche, wie bei dem einzelnen Abruf eines Anhanges handelt?
+app.get('/anhang/:ID', function (req, res) { //Suchfunktion in der Anhangsliste
+    db.get('anhang:'+req.params.id, function(err, rep){
+        if(rep){
+            res.type('json').send(rep);
+        }
+        else{
+            res.status(404).type('text').send(Dieser Anhang wurde nicht gefunden.);
+        }
+    });
+});
+
 
 app.delete('/notiz/:ID', function (req, res) { //Löschen einer Notiz
- notiz.del(req.params.id);
-  res.send(200);
+    db.get('notiz:'+req.params.id, function(err, rep){
+        if(rep){
+            db.del('notiz:'+req.params.id, function(err, rep){
+                res.type('text').send('Notiz wurde gelöscht.');
+            });
+        }
+        else {
+            res.status(404).type('text').send('Notiz nicht gefunden');
+        }
+    });
 });
 
 
