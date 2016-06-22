93785f03a9c8357013ecc0d4001f11f13d41a33a
Mirrorfy <daniela.meyer@smail.fh-koeln.de>
2016-06-22 17:19:32 +0200

    create app.js

---
+++ Dienstnutzer/app.js
@@ -0,0 +1,9 @@
+var express = require ('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var ejs = require('ejs');
+var fs = require('fs');
+var http = require('http');
+
+var app = express();
+
